a37a0a574bb337f4dc129c05015fd7b458e50fe3
HaoWen2003 <haowen2003@hotmail.com>
2022-11-11 00:24:40 +0800

    Student.cs

--- IOOP Assignment/Student.cs
+++ IOOP Assignment/Student.cs
@@ -9,12 +9,34 @@ namespace IOOP_Assignment
     internal class Student
     {
         private string name;
-        private string stuID;
+        private string studentID;
+        private string level;
+        private string IC;
+        private string address;
+        private string email;
+        private string contactNum;
+        private string sub1;
+        private string sub2; 
+        private string sub3;
 
-        public Student(string name,string stuID)
+        public Student(string name,string studentID)
         {
             this.name = name;
-            this.stuID = stuID;
+            this.studentID = studentID;
         }
+
+        public Student(string name, string studentID, string level, string iC, string address, string email, string contactNum, string sub1, string sub2, string sub3) : this(name, studentID)
+        {
+            this.level = level;
+            IC = iC;
+            this.address = address;
+            this.email = email;
+            this.contactNum = contactNum;
+            this.sub1 = sub1;
+            this.sub2 = sub2;
+            this.sub3 = sub3;
+        }   
+
+
     }
 }
